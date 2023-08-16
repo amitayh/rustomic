f1b7de7803b1b2a04dd6196938e1250e3602ed40
Amitay Horwitz <amitayh@gmail.com>
2023-08-16 07:51:52 +0300

    wip

--- src/storage/disk.rs
+++ src/storage/disk.rs
@@ -1,17 +1,30 @@
 use crate::storage::*;
 
-pub struct DiskStorage {}
+pub struct DiskStorage {
+    db: rocksdb::DB
+}
 
 impl Storage for DiskStorage {
-    fn save(&mut self, _datoms: &[Datom]) -> Result<(), StorageError> {
-        todo!()
+    fn save(&mut self, datoms: &[Datom]) -> Result<(), StorageError> {
+        let mut batch = rocksdb::WriteBatch::default();
+        for datom in datoms {
+            batch.put(datom.entity.to_be_bytes(), b"foo");
+        }
+        self.db.write(batch).unwrap();
+        Ok(())
     }
 
     fn find_datoms(&self, _clause: &Clause, _tx_range: u64) -> Result<Vec<Datom>, StorageError> {
-        todo!()
+        Ok(vec![])
     }
 
     fn resolve_ident(&self, _ident: &str) -> Result<EntityId, StorageError> {
         todo!()
+    }
+}
+
+impl DiskStorage {
+    pub fn new(db: rocksdb::DB) -> Self {
+        DiskStorage { db }
     }
 }
